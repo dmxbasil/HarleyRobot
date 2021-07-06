0953f20269e54b01880973a5e6b8387de7c2223f
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-07 00:52:27 +0530

    Update extraction.py

--- HarleyRobot/modules/helper_func/extraction.py
+++ HarleyRobot/modules/helper_func/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from LaylaRobot import LOGGER
-from LaylaRobot.modules.users import get_user_id
+from HarleyRobot import LOGGER
+from HarleyRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
