0b141a1863c132b4999ddd4e25261722a6e24741
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-07 00:54:52 +0530

    Update handlers.py

--- HarleyRobot/modules/helper_func/handlers.py
+++ HarleyRobot/modules/helper_func/handlers.py
@@ -1,6 +1,6 @@
-import LaylaRobot.modules.sql.blacklistusers_sql as sql
-from LaylaRobot import ALLOW_EXCL
-from LaylaRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import HarleyRobot.modules.sql.blacklistusers_sql as sql
+from HarleyRobot import ALLOW_EXCL
+from HarleyRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
