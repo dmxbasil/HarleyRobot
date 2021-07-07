02711c3d1f84a3518a0788a792177d352c6e0a21
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-08 01:46:10 +0530

    Update nsfw_sql.py

--- HarleyRobot/modules/sql/nsfw_sql.py
+++ HarleyRobot/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from LaylaRobot.modules.sql import BASE, SESSION
+from HarleyRobot.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
 #   |        Kang with Credits         |
