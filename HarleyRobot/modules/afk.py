7024287557d2c48c50c4da01f9cc0a99549914d6
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-11 23:24:36 +0530

    Update afk.py

--- HarleyRobot/modules/afk.py
+++ HarleyRobot/modules/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import (
+from HarleyRobot import dispatcher
+from HarleyRobot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from LaylaRobot.modules.sql import afk_sql as sql
-from LaylaRobot.modules.users import get_user_id
+from HarleyRobot.modules.sql import afk_sql as sql
+from HarleyRobot.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
