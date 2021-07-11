8efd38bd43e4de5f9f50be3e5ea270f8a65eb69e
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-11 14:52:37 +0530

    Update __imdb.py

--- HarleyRobot/modules/__imdb.py
+++ HarleyRobot/modules/__imdb.py
@@ -1,11 +1,11 @@
-from LaylaRobot import telethn as tbot
+from HarleyRobot import telethn as tbot
 import os
 import re
 import bs4
 import requests
 from telethon import types
 from telethon.tl import functions
-from LaylaRobot.events import register
+from HarleyRobot.events import register
 
 langi = "en"
 
