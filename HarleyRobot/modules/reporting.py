b1253195a971179d5a9403ffabc0c054e761aa03
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-13 00:27:01 +0530

    Update reporting.py

--- HarleyRobot/modules/reporting.py
+++ HarleyRobot/modules/reporting.py
@@ -1,9 +1,9 @@
 import html
 
-from LaylaRobot import LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from LaylaRobot.modules.log_channel import loggable
-from LaylaRobot.modules.sql import reporting_sql as sql
+from HarleyRobot import LOGGER, DRAGONS, TIGERS, WOLVES, dispatcher
+from HarleyRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from HarleyRobot.modules.log_channel import loggable
+from HarleyRobot.modules.sql import reporting_sql as sql
 from telegram import Chat, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
