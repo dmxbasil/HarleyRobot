9d7318e16ccc2f802761086240150838fd78a8a0
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-14 01:54:34 +0530

    Update ping.py

--- HarleyRobot/modules/ping.py
+++ HarleyRobot/modules/ping.py
@@ -5,9 +5,9 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from LaylaRobot import StartTime, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import sudo_plus
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from HarleyRobot import StartTime, dispatcher
+from HarleyRobot.modules.helper_funcs.chat_status import sudo_plus
+from HarleyRobot.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
