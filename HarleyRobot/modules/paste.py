e2a25b03bef3d586795c12b88c0802fa6bb23d4b
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-14 00:44:47 +0530

    Update paste.py

--- HarleyRobot/modules/paste.py
+++ HarleyRobot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from HarleyRobot import dispatcher
+from HarleyRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
