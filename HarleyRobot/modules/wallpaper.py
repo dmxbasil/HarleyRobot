58413764a7989f969cda53bda9d5244256d7ed65
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-15 00:37:42 +0530

    Update wallpaper.py

--- HarleyRobot/modules/wallpaper.py
+++ HarleyRobot/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from LaylaRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler
+from HarleyRobot import SUPPORT_CHAT, WALL_API, dispatcher
+from HarleyRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
