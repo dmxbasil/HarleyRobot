7400d4daa8b42f37a8a05e3f00798afa265d3653
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2021-07-14 13:02:02 +0530

    Update shell.py

--- HarleyRobot/modules/shell.py
+++ HarleyRobot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from LaylaRobot import LOGGER, dispatcher
-from LaylaRobot.modules.helper_funcs.chat_status import dev_plus
+from HarleyRobot import LOGGER, dispatcher
+from HarleyRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
